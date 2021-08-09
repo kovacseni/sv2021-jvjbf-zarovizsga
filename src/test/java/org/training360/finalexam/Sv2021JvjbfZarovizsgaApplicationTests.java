dc558d79286bfbafff876572c40052265382596d
kovacseni <kovacseni@gmail.com>
2021-08-09 14:50:10 +0200

    Initial commit

---
+++ src/test/java/org/training360/finalexam/Sv2021JvjbfZarovizsgaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training360.finalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sv2021JvjbfZarovizsgaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
